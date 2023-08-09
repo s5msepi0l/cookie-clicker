4c93da6501a277c658fa59fa536d8f54a99b2552
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2023-08-09 13:25:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
-const cookie = document.getElementById('cookie');
-let score = 0;
+const up_pn = document.getElementById("pn");
+const up_csa = document.getElementById("csa");
+const up_lye = document.getElementById("lye");
+const up_sbc = document.getElementById("sbc");
 
-const showScore = document.getElementById('score');
+const cookie = document.getElementById('cookie');
+
+let money = 0;
+let money_gen = 1;
+
+const showScore = document.getElementById('amt');
+const level = document.getElementById("level");
+
 cookie.addEventListener('click', ()=>{
-    score++;
-    showScore.innerHTML= score;
-    animate_bmb(cookie, 0.2, 20);
+    money += money_gen;
+    showScore.innerHTML= money;
+    animate_bmb(cookie, 0.2, 10);
     
-    if(score == 20){
+    if(money == 20){
         alert("Win_condition hit");
+    }
+});
+
+// upgrade boilerplate
+const green = "rgb(37, 242, 10)";
+const red ="rgb(242, 10, 49)";
+
+up_pn.addEventListener("click", ()=> {
+    if (money >= 100) {
+        up_pn.style.backgroundColor = green;
+        money_gen += 5;
+        money -= 100;
+        showScore.innerHTML = money;
+        level.innerHTML = money_gen;
+    }
+
+});
+
+up_sbc.addEventListener("click", ()=> {
+    if (money >= 5000) {
+        up_sbc.style.backgroundColor = green;
+        money_gen += 500;
+        money -= 5000;
+        showScore.innerHTML = money;
+        level.innerHTML = money_gen;
+    }
+});
+
+up_lye.addEventListener("click", ()=> {
+    if (money >= 17.63) {
+        up_lye.style.backgroundColor = green;
+        money_gen += 1;
+        money -= 17.63;
+        showScore.innerHTML = money;
+        level.innerHTML = money_gen;
+    }
+});
+
+up_csa.addEventListener("click", ()=> {
+    if (money >= 500) {
+        up_csa.style.backgroundColor = green;
+        money_gen += 50;
+        money -= 500;
+        showScore.innerHTML = money;
+        level.innerHTML = money_gen;
     }
 });
 
@@ -39,11 +93,11 @@ function sleep(ms) {
 async function animate_bmb(Src, amt, animation_cycles) { // janky ass diy animation system
     for (let i = 0; i<animation_cycles; i++) {
         Src.style.padding = Src.style.padding = `${i * amt / 1}rem`; 
-        await sleep(1);
+        await sleep(0.1);
     }
 
     for (let i = animation_cycles; i>3; i--) {
         Src.style.padding = Src.style.padding = `${i * amt / 1}rem`; 
-        await sleep(1);
+        await sleep(0.1);
     }
 }
